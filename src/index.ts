eef50596ee4b391481f574cce9a88749bd3968fa
jpb <jpb.06@outlook.com>
2018-09-28 21:22:44 +0200

    Export related

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-﻿console.log('Hello world');
+﻿import { DalConfiguration as Configuration } from './dal/configuration/dal.configuration';
+import { CryptoService } from './crypto/crypto.service';
+import * as Types from './types.export';
+
+export { Configuration, CryptoService, Types };
---
+++ src/types.export.ts
@@ -0,0 +1,1 @@
+﻿export * from './dal/types/exported.types';
