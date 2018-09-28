69ee81834889db37296a0b32c7ea70fb0e8bbeb7
jpb <jpb.06@outlook.com>
2018-09-28 21:20:52 +0200

    Data access layer

---
+++ src/dal/manipulation/keypair.store.ts
@@ -0,0 +1,74 @@
+﻿import * as mongoose from 'mongoose';
+
+import { KeyPairModel } from './../types/persisted.types';
+import { KeyPair } from './../types/exported.types';
+import { DalConfiguration } from './../configuration/dal.configuration';
+
+export abstract class KeyPairStore {
+    public static async Save(
+        data: KeyPair
+    ): Promise<boolean> {
+        await mongoose.connect(DalConfiguration.GetURI());
+        let db = mongoose.connection;
+
+        try {
+            const Model = new KeyPairModel().getModelForClass(KeyPairModel);
+
+            let keyPair = new Model({
+                application: data.application,
+                dateGenerated: data.dateGenerated,
+                publicKey: data.publicKey,
+                privateKey: data.privateKey
+            });
+
+            await keyPair.save();
+
+            return true;
+        } finally {
+            db.close();
+        }
+    }
+
+    public static async GetAll(
+        application: string
+    ): Promise<Array<KeyPair>>  {
+        await mongoose.connect(DalConfiguration.GetURI());
+        let db = mongoose.connection;
+
+        try {
+            const Model = new KeyPairModel().getModelForClass(KeyPairModel);
+
+            let data = await Model
+                .find({ 'application': application })
+                .select('application privateKey publicKey dateGenerated')
+                .sort('-dateGenerated')
+                .exec();
+
+            return data.map(el => el.asExportedType());
+
+        } finally {
+            db.close();
+        }
+    }
+
+    public static async RemoveOldest(
+        application: string
+    ): Promise<boolean> {
+        await mongoose.connect(DalConfiguration.GetURI());
+        let db = mongoose.connection;
+
+        try {
+            const Model = new KeyPairModel().getModelForClass(KeyPairModel);
+
+            let data = await Model
+                .findOne()
+                .sort('-dateGenerated')
+                .remove()
+                .exec();
+    
+            return true;
+        } finally {
+            db.close();
+        }
+    }
+}
