eaab925af65047d61d5a29efcc8e7cadf2c64dc8
jpb <jpb.06@outlook.com>
2018-09-28 21:21:18 +0200

    Configuration for data access layer

---
+++ src/dal/configuration/dal.configuration.ts
@@ -0,0 +1,32 @@
+﻿export abstract class DalConfiguration {
+    private static url: string;
+    private static database: string;
+
+    public static Setup(
+        url: string,
+        database: string
+    ): void {
+        this.url = url;
+        this.database = database;
+    }
+
+    public static Verify(): void {
+        if (this.url === undefined || this.url.length === 0) {
+            throw new Error('No url specified to access mongodb instance');
+        }
+
+        if (this.database === undefined || this.url.length === 0) {
+            throw new Error('No database specified');
+        }
+    }
+
+    public static SwitchDatabase(database: string): void {
+        this.database = database;
+    }
+
+    public static GetURI(): string {
+        this.Verify();
+
+        return 'mongodb://'+this.url + '/' + this.database;
+    }
+}
