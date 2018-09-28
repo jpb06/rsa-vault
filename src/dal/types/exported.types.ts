5f0b2e05d3a926558858c78645acbc88272f2a80
jpb <jpb.06@outlook.com>
2018-09-28 21:20:33 +0200

    Setting up types (Internal & external)

---
+++ src/dal/types/exported.types.ts
@@ -0,0 +1,29 @@
+﻿/* ---------------------------------------------------------------------------------------------------------------
+   KeyPair
+   ---------------------------------------------------------------------------------------------------------------*/
+export class KeyPair {
+    application: string;
+    privateKey: string;
+    publicKey: string;
+    dateGenerated: string;
+
+    constructor(
+        application: string,
+        privateKey: string,
+        publicKey: string,
+        dateGenerated: string
+    ) {
+        this.application = application;
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+        this.dateGenerated = dateGenerated;
+    }
+
+    public forApplication(
+        application: string
+    ): KeyPair {
+        this.application = application;
+
+        return this;
+    }
+}
---
+++ src/dal/types/persisted.types.ts
@@ -0,0 +1,26 @@
+﻿import { prop, Typegoose, instanceMethod } from 'typegoose';
+import { KeyPair } from './exported.types';
+
+/* ---------------------------------------------------------------------------------------------------------------
+   KeyPair
+   ---------------------------------------------------------------------------------------------------------------*/
+export class KeyPairModel extends Typegoose {
+    @prop()
+    application: string;
+    @prop()
+    privateKey: string;
+    @prop()
+    publicKey: string;
+    @prop()
+    dateGenerated: string;
+
+    @instanceMethod
+    public asExportedType(): KeyPair {
+        return new KeyPair(
+            this.application,
+            this.privateKey,
+            this.publicKey,
+            this.dateGenerated
+        );
+    }
+}
