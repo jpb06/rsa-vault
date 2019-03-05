b4111827b7f6d5d7a76ea379927859cb918990f9
jpb <jpb.06@outlook.com>
2019-03-05 18:47:01 +0100

    Redefining build tasks from the ground

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,18 @@
-﻿/// <binding BeforeBuild='clean' AfterBuild='generatePackage, moveReadme' Clean='clean' />
+﻿/// <binding BeforeBuild='clean' AfterBuild='generate-package, move-readme' Clean='clean' />
 const gulp = require('gulp');
-const rimraf = require('rimraf');
-const fs = require('fs');
 
-gulp.task('generatePackage', () => {
-    const package = JSON.parse(fs.readFileSync('./package.json').toString());
+const generatePackage = require('./project-apparatus/tasks/generate.package.task.js');
+const cleanDist = require('./project-apparatus/tasks/clean.dist.task.js');
+const moveReadme = require('./project-apparatus/tasks/move.readme.task.js');
 
-    let distPackage = {
-        name: package.name,
-        version: package.version,
-        description: package.version,
-        main: package.main,
-        types: package.types,
-        author: package.author,
-        dependencies: package.dependencies
-    };
-
-    fs.writeFile('./dist/package.json', JSON.stringify(distPackage, null, 2), 'utf8', (err) => {
-        if (err) console.log('Error while writing dist package.json file:', err);
-    });
+gulp.task('generate-package', async () => {
+    await generatePackage();
 });
 
-gulp.task('moveReadme', () => {
-    fs.unlink('./dist/README.md', function (err) {
-        if (err === null || (err && err.code === 'ENOENT')) {
-            fs.createReadStream('./README.md').pipe(fs.createWriteStream('./dist/README.md'));
-        } else if (err) {
-            console.error('Error while moving readme:', err);
-        }
-    });
+gulp.task('move-readme', async () => {
+    await moveReadme();
 });
 
-gulp.task('clean', () => {
-    rimraf('./dist', (err) => {
-        console.log('Error during clean:', err);
-    });
+gulp.task('clean', async () => {
+    await cleanDist();
 });
---
+++ project-apparatus/tasks/move.readme.task.js
@@ -0,0 +1,13 @@
+﻿const fs = require('fs-extra');
+
+const consoleUtil = require('./../util/console.util.js');
+
+cleanDist = async function () {
+    consoleUtil.printHeader('Moving readme ...');
+
+    await fs.copy('./README.md', './dist/README.md');
+
+    console.log('Done.');
+};
+
+module.exports = cleanDist;
