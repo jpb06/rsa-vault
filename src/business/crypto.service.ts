7c1bf73d823632b032d9c3eebc8f62b709bf8261
jpb <jpb.06@outlook.com>
2019-03-05 18:48:50 +0100

    Moving crypto logic to its own file

---
+++ src/business/crypto.service.ts
@@ -0,0 +1,21 @@
+﻿import * as moment from 'moment';
+import * as NodeRSA from 'node-rsa';
+
+import { ApplicationKeys } from '../dal/types/exported.types';
+
+export function generateRSAKeys(): ApplicationKeys {
+    const key = new NodeRSA();
+
+    key.generateKeyPair(2048, 65537);
+
+    const publicKey = key.exportKey('pkcs1-public-pem');
+    const privatekey = key.exportKey('pkcs1-private-pem');
+
+    return new ApplicationKeys(
+        '',
+        privatekey,
+        publicKey,
+        moment().format(),
+        ''
+    );
+}
