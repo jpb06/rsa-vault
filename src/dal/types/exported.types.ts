96f47ccf0a95c0e769b5dde4cc76448d099756a4
jpb <jpb.06@outlook.com>
2019-03-05 18:48:04 +0100

    Now exporting an ApplicationKeys object containing the previously generated public key

--- src/dal/types/exported.types.ts
+++ src/dal/types/exported.types.ts
@@ -1,27 +1,32 @@
 ﻿/* ---------------------------------------------------------------------------------------------------------------
-   KeyPair
+   Application keys
    ---------------------------------------------------------------------------------------------------------------*/
-export class KeyPair {
+export class ApplicationKeys {
     application: string;
+
     privateKey: string;
     publicKey: string;
     dateGenerated: string;
+
+    previousPublicKey: string;
 
     constructor(
         application: string,
         privateKey: string,
         publicKey: string,
-        dateGenerated: string
+        dateGenerated: string,
+        previousPublicKey: string
     ) {
         this.application = application;
         this.privateKey = privateKey;
         this.publicKey = publicKey;
         this.dateGenerated = dateGenerated;
+        this.previousPublicKey = previousPublicKey;
     }
 
     public forApplication(
         application: string
-    ): KeyPair {
+    ): ApplicationKeys {
         this.application = application;
 
         return this;
