a7ad1e327de5ee71a6623d881aa1eb82144244c2
jpb <jpb.06@outlook.com>
2019-10-03 21:33:47 +0200

    jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    testEnvironment: 'node',
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    testRegex: "(/tests/units/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testPathIgnorePatterns: ["/dist/", "/node_modules/"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    collectCoverage: true
+};
