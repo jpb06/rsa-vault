c072738470438eadeafeafd79abeb1c64453ef6e
jpb <jpb.06@outlook.com>
2019-10-04 06:03:57 +0200

    Adding a test for cache usage

--- src/tests/units/business/vault.service.test.ts
+++ src/tests/units/business/vault.service.test.ts
@@ -42,7 +42,7 @@ describe('Vault service', () => {
     });
 
     it('should get me a new key pair when dbase has only outdated ones', async () => {
-        jest.setTimeout(20000);
+        jest.setTimeout(30000);
         const app = 'vaultdbaseoutdated';
 
         await initAndClear(app);
@@ -60,5 +60,42 @@ describe('Vault service', () => {
 
         const persistedKeys = await KeyPairStore.GetAll(app);
         expect(persistedKeys.length).toBe(2);
+    });
+
+    it('should get me a key pair from cache', async () => {
+        jest.setTimeout(30000);
+        const app = 'vaultfromcache';
+
+        await initAndClear(app);
+        CacheService.Clear();
+
+        let keys = generateRSAKeys().forApplication(app);
+        keys.dateGenerated = moment().add(-10, 'd').format();
+        let result1 = await KeyPairStore.Save(keys);
+        expect(result1).toBe(true);
+
+        let fetchedKeys = await VaultService.GetKeyPair(app);
+
+        verifyEncryptionDecryption(fetchedKeys.privateKey);
+
+        const result2 = await KeyPairStore.RemoveAll(app);
+        expect(result2).toBe(true);
+
+        let keys2 = generateRSAKeys().forApplication(app);
+        keys.dateGenerated = moment().add(-5, 'd').format();
+        let result3 = await KeyPairStore.Save(keys2);
+        expect(result3).toBe(true);
+
+        fetchedKeys = await VaultService.GetKeyPair(app);
+
+        verifyEncryptionDecryption(fetchedKeys.privateKey);
+        verifyKeys(fetchedKeys, keys);
+
+        const persistedKeys = await KeyPairStore.GetAll(app);
+        expect(persistedKeys.length).toBe(1);
+
+        const cachedKeys = await CacheService.GetKeyPairs(app);
+        expect(persistedKeys.length).toBe(1);
+        verifyKeys(cachedKeys[0], keys);
     });
 });
